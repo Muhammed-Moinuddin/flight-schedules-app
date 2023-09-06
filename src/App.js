e7d1cf42e484515daa01df0dbf1855d0b2291407
Muhammad Moinuddin <moinuddinilyas786@gmail.com>
2023-09-06 23:43:34 +0500

    Updated App.js
    Made small changes and rendered data in a <p> tag

--- src/App.js
+++ src/App.js
@@ -6,39 +6,50 @@ import Form from 'react-bootstrap/Form';
 function App() {
 
   const [endPoint, setEndPoint] = useState('');
+  const [container, setContainer] = useState([]);
 
   useEffect(() => {
-    async function fetchData(){
-      const url = 'https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr';
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': 'bb2751c7bdmshce56e8660c931f4p19b039jsn2ffe4b7ac46a',
-          'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-        }
-  };
-      try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-      fetchData();
-    }, [])
+    fetchData();
+  }, [endPoint])
 
+  const fetchData = async() => {
+    const url = `https://online-movie-database.p.rapidapi.com/auto-complete?q=+${endPoint}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'bb2751c7bdmshce56e8660c931f4p19b039jsn2ffe4b7ac46a',
+        'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+      }
+};
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setContainer(result.d);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <div className="App">
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3">
           <Form.Control type="text" value={endPoint} onChange={e => setEndPoint(e.target.value)} placeholder="Search here.." />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" >
             Submit
           </Button>
       </Form>
+      {container.map((item) => {
+          return(
+            <p>{item.l}</p>
+          )
+      })}
     </div>
   );
 }
